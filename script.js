e46cc5f33213091a7fe6ec738e4e09a5640f6d74
Cleyton A. L <souaquelecleyton@yandex.com>
2021-08-16 17:36:35 -0300

    Keys paint red when pressed

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 window.addEventListener('keydown', function(e){
-    var keyName = e.key;
+    var keyName = e.key == "\\" ? "Barra" : e.key;
     var divKeyPressed = document.querySelectorAll(`[data-keyName="${keyName}"]`);
-    var actualPressed = document.querySelectorAll('.active');
-
-    console.log(keyName)
-
-    actualPressed.forEach(key => { key.classList.remove('active') })
+    
     divKeyPressed.forEach(key => { key.classList.add('active') })
+})
+
+window.addEventListener('keyup', function(){
+    var actualPressed = document.querySelectorAll('.active');
+    actualPressed.forEach(key => { key.classList.remove('active') })
 })
